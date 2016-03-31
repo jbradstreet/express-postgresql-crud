5abf3bd11f47672c90cd9933f70deb01f46c112f
Jenni Bradstreet <jennic15@gmail.com>
2016-03-31 15:31:29 -0700

    Add put request.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 
 var routes = require('./routes/index');
@@ -18,6 +19,7 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(methodOverride('_method'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- routes/albums.js
+++ routes/albums.js
@@ -13,7 +13,7 @@ router.get('/albums', function(req, res, next) {
 });
 
 router.get('/albums/new', function(req, res, next) {
-  res.render('albums/new');
+  res.render('albums/new', {album: {}});
 });
 
 router.get('/albums/:index', function(req, res, next) {
@@ -23,10 +23,26 @@ router.get('/albums/:index', function(req, res, next) {
   });
 });
 
+router.get('/albums/:id/edit', function(req, res, next) {
+  var id = req.params.id;
+  Albums().where({ id: id }).first().then(function(results) {
+    res.render('albums/edit', { album: results });
+  });
+});
+
 router.post('/albums', function(req, res, next) {
-  Albums().insert({ name: req.body.album_name, artist: req.body.artist_name, genre: req.body.genre }).then(function () {
+  Albums().insert({ name: req.body.album, artist: req.body.artist, genre: req.body.genre, stars: req.body.rating}).then(function (result) {
     res.redirect('/albums');
   });
 });
+
+router.post('/albums/:id/edit', function(req, res, next) {
+  var id = req.params.id;
+  Albums().update({ name: req.body.album, artist: req.body.artist, genre: req.body.genre, stars: req.body.rating }).where({id: id}).then(function (result) {
+    res.redirect('/albums/');
+  });
+});
+
+
 
 module.exports = router;

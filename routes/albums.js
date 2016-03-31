c9bf617c6397af134e0b52fcf3443f391ffbe2e9
Jenni Bradstreet <jennic15@gmail.com>
2016-03-31 13:48:53 -0700

    add album route

--- routes/albums.js
+++ routes/albums.js
@@ -16,8 +16,15 @@ router.get('/albums/new', function(req, res, next) {
   res.render('albums/new');
 });
 
+router.get('/albums/:index', function(req, res, next) {
+  var index =  req.params.index;
+  Albums().where({ id: index }).first().then(function(results) {
+    res.render(`albums/album`, { album: results });
+  });
+});
+
 router.post('/albums', function(req, res, next) {
-  Albums().insert({ name: req.body.album_name }).then(function () {
+  Albums().insert({ name: req.body.album_name, artist: req.body.artist_name, genre: req.body.genre }).then(function () {
     res.redirect('/albums');
   });
 });
